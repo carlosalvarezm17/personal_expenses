3402aa1407ee5f8fbdd3259062114cc396c3d8f3
Carlos Alvarez Moreno <ceam1710@gmail.com>
2019-10-30 13:35:46 -0500

    Transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  final List<Transaction> transaction = [
+    Transaction(id: 't1', title: 'SRI', amount: 60, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Food', amount: 20, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +25,7 @@ class MyApp extends StatelessWidget {
               Container(
                 width: double.infinity,
                 child: Card(
-                  child: Container(child: Text('Chart'), width: double.infinity,),
+                  child: Container(child: Text('Chart'),),
                   color: Colors.blue,
                   elevation: 5,
                 ),
