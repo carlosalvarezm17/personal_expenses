8b9b7045e9cbd3eb5870c65d9499a706d241522e
Carlos Alvarez Moreno <ceam1710@gmail.com>
2019-10-31 14:04:12 -0500

    Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,45 @@ import 'package:personal_expenses/widgets/user_transaction.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Material App Bar'),
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: double.infinity,
-                  child: Card(
-                    child: Container(
-                      child: Text('Chart'),
-                    ),
-                    color: Colors.blue,
-                    elevation: 5,
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: (){},
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  child: Container(
+                    child: Text('Chart'),
                   ),
+                  color: Colors.blue,
+                  elevation: 5,
                 ),
-                UserTransaction(),
-                
-              ],
-            ),
-          )),
+              ),
+              UserTransaction(),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
