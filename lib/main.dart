54e7da10798ec417587425ce7c157788a5ebb7f8
Carlos Alvarez Moreno <ceam1710@gmail.com>
2019-11-11 14:10:08 -0500

    Extracting Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:personal_expenses/models/transaction.dart';
 import 'package:personal_expenses/widgets/chart.dart';
