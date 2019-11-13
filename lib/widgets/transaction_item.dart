fb83c98fc0bbafc04b180da2a212c9bedb56b525
Carlos Alvarez Moreno <ceam1710@gmail.com>
2019-11-13 09:35:34 -0500

    A Problem with Lists & Stateful Widgets

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:personal_expenses/models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.userTransaction,
@@ -11,6 +13,25 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction userTransaction;
   final Function deleteTransaction;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red, Colors.black, Colors.blue, Colors.purple,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)];
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,34 +43,35 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6.0),
             child: FittedBox(
               child: Text(
-                '\$${userTransaction.amount.toStringAsFixed(2)}',
+                '\$${widget.userTransaction.amount.toStringAsFixed(2)}',
               ),
             ),
           ),
         ),
         title: Text(
-          userTransaction.title,
+          widget.userTransaction.title,
           style: Theme.of(context).textTheme.title,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(userTransaction.date),
+          DateFormat.yMMMd().format(widget.userTransaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 400
             ? FlatButton.icon(
                 icon: Icon(Icons.delete),
                 label: Text('Delete'),
                 textColor: Theme.of(context).errorColor,
-                onPressed: () => deleteTransaction(userTransaction.id),
+                onPressed: () => widget.deleteTransaction(widget.userTransaction.id),
               )
             : IconButton(
                 icon: Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTransaction(userTransaction.id),
+                onPressed: () => widget.deleteTransaction(widget.userTransaction.id),
               ),
       ),
     );
